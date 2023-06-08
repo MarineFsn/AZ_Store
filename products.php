a9f01118770389dc4cd79d5ad6d0283912e7eb17
ThomMoerman <thomas.moerman.7@gmail.com>
2023-06-08 10:45:11 +0200

    Add header.php

---
+++ products.php
@@ -0,0 +1,16 @@
+<?php
+$items = [
+    [
+        'id' => 1,
+        'product' => 'Nike Air Max 270',
+        'price' => 140,
+        'image_url' => '/Assets/img/shoe_one.png', 
+    ],
+    [
+        'id' => 2,
+        'product' => 'Nike Air Max 409',
+        'price' => 160,
+        'image_url' => '/Assets/img/shoe_two.png',
+    ]
+];
+?> 
